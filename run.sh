376f4f9ae03aab41bcf4d65f5fa83fa763c6c28f
Geri Madanguit <gemadang@syr.edu>
2018-07-20 13:57:05 -0700

    added ssh line to run

--- run.sh
+++ run.sh
@@ -23,4 +23,16 @@ aws s3 sync s3://japan-roof-top-bucket /storage --no-sign-request
 #ls -la /storage/Train_label
 #ls -la /storage/Train_data
 
+apt-get update && apt-get install -y openssh-server
+mkdir /var/run/sshd
+echo 'root:password' | chpasswd
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+# SSH login fix. Otherwise user is kicked off after login
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+
+echo "export VISIBLE=now" >> /etc/profile
+
+/usr/sbin/sshd -D
+
 python model.py
